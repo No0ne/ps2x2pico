a40f132312965cc26555556495671df97b158578
No0ne <github@exa.solar>
2022-08-24 11:12:24 +0200

    tinyusb compiled

--- ps2x2pico.c
+++ ps2x2pico.c
@@ -1,63 +1,42 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/uart.h"
+//#include <stdio.h>
+//#include "pico/stdlib.h"
+//#include "hardware/uart.h"
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <string.h>
+
 #include "hardware/gpio.h"
-#include "hardware/divider.h"
 #include "hardware/pio.h"
+#include "bsp/board.h"
+#include "tusb.h"
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
+/*#define UART_ID uart1
 #define BAUD_RATE 9600
-
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define UART_TX_PIN 4
-#define UART_RX_PIN 5
+#define UART_RX_PIN 5*/
 
-// GPIO defines
-// Example uses GPIO 2
-#define GPIO 2
-
-
-
-
-int main()
-{
-    stdio_init_all();
-
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+void main() {
+  board_init();
+  printf("TinyUSB Host CDC MSC HID Example\r\n");
+  
+  /*stdio_init_all();
+  uart_init(UART_ID, BAUD_RATE);
+  gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+  gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+  puts("Hello, world!");*/
+  
+  tusb_init();
+  
+  while(true) {
+    tuh_task();
     
+  }
+}
 
-    // GPIO initialisation.
-    // We will make this GPIO an input, and pull it up by default
-    gpio_init(GPIO);
-    gpio_set_dir(GPIO, GPIO_IN);
-    gpio_pull_up(GPIO);
-    
+void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len) {
 
-    // Example of using the HW divider. The pico_divider library provides a more user friendly set of APIs 
-    // over the divider (and support for 64 bit divides), and of course by default regular C language integer
-    // divisions are redirected thru that library, meaning you can just use C level `/` and `%` operators and
-    // gain the benefits of the fast hardware divider.
-    int32_t dividend = 123456;
-    int32_t divisor = -321;
-    // This is the recommended signed fast divider for general use.
-    divmod_result_t result = hw_divider_divmod_s32(dividend, divisor);
-    printf("%d/%d = %d remainder %d\n", dividend, divisor, to_quotient_s32(result), to_remainder_s32(result));
-    // This is the recommended unsigned fast divider for general use.
-    int32_t udividend = 123456;
-    int32_t udivisor = 321;
-    divmod_result_t uresult = hw_divider_divmod_u32(udividend, udivisor);
-    printf("%d/%d = %d remainder %d\n", udividend, udivisor, to_quotient_u32(uresult), to_remainder_u32(uresult));
+}
 
+void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
 
-    puts("Hello, world!");
-
-    return 0;
 }
--- tusb_config.h
+++ tusb_config.h
@@ -30,48 +30,17 @@
  extern "C" {
 #endif
 
-//--------------------------------------------------------------------+
-// Board Specific Configuration
-//--------------------------------------------------------------------+
-
-// RHPort number used for host can be defined by board.mk, default to port 0
-#ifndef BOARD_TUH_RHPORT
-#define BOARD_TUH_RHPORT      0
-#endif
-
-// RHPort max operational speed can defined by board.mk
-#ifndef BOARD_TUH_MAX_SPEED
-#define BOARD_TUH_MAX_SPEED   OPT_MODE_DEFAULT_SPEED
-#endif
-
 //--------------------------------------------------------------------
 // COMMON CONFIGURATION
 //--------------------------------------------------------------------
 
-// defined by compiler flags for flexibility
-#ifndef CFG_TUSB_MCU
-#error CFG_TUSB_MCU must be defined
-#endif
+#define CFG_TUSB_MCU          OPT_MCU_RP2040
+#define CFG_TUSB_RHPORT0_MODE OPT_MODE_HOST
 
 #ifndef CFG_TUSB_OS
 #define CFG_TUSB_OS           OPT_OS_NONE
 #endif
 
-#ifndef CFG_TUSB_DEBUG
-#define CFG_TUSB_DEBUG        0
-#endif
-
-// Enable Host stack, Default is max speed that hardware controller could support with on-chip PHY
-#define CFG_TUH_ENABLED       1
-#define CFG_TUH_MAX_SPEED     BOARD_TUH_MAX_SPEED
-
-/* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
- * Tinyusb use follows macros to declare transferring memory so that they can be put
- * into those specific section.
- * e.g
- * - CFG_TUSB_MEM SECTION : __attribute__ (( section(".usb_ram") ))
- * - CFG_TUSB_MEM_ALIGN   : __attribute__ ((aligned(4)))
- */
 #ifndef CFG_TUSB_MEM_SECTION
 #define CFG_TUSB_MEM_SECTION
 #endif
@@ -84,14 +53,12 @@
 // CONFIGURATION
 //--------------------------------------------------------------------
 
-// Size of buffer to hold descriptors and other data used for enumeration
 #define CFG_TUH_ENUMERATION_BUFSIZE 256
 
 #define CFG_TUH_HUB                 1
-// max device support (excluding hub device)
-#define CFG_TUH_DEVICE_MAX          (CFG_TUH_HUB ? 4 : 1) // hub typically has 4 ports
+#define CFG_TUH_DEVICE_MAX          4
 
-#define CFG_TUH_HID                  4
+#define CFG_TUH_HID                 4
 #define CFG_TUH_HID_EPIN_BUFSIZE    64
 #define CFG_TUH_HID_EPOUT_BUFSIZE   64
 
